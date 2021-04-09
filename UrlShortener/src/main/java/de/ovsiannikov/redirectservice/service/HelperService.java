5be9c5e674c0f33263f33ea85fc5b7973d13677a
Ovsiannikov78 <seri_tanya@mail.ru>
2021-04-09 07:13:09 +0200

    Changed the @Future annotation message in LongUrlDto a more appropriate one.
    Removed formatter from HelperService.

--- UrlShortener/src/main/java/de/ovsiannikov/redirectservice/service/HelperService.java
+++ UrlShortener/src/main/java/de/ovsiannikov/redirectservice/service/HelperService.java
@@ -1,13 +1,10 @@
 package de.ovsiannikov.redirectservice.service;
 
 import de.ovsiannikov.redirectservice.dao.UrlRepository;
-import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.stereotype.Service;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Base64;
-import java.util.Locale;
 
 
 @Service
@@ -16,8 +13,6 @@ public class HelperService {
     private static final SecureRandom random = new SecureRandom();
     private static final Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
 
-    private final UrlValidator validator = new UrlValidator(new String[]{"http", "https"});
-
     private final UrlRepository urlRepository;
 
     public HelperService(UrlRepository urlRepository) {
@@ -25,18 +20,17 @@ public class HelperService {
     }
 
     public String generateShortUrl() {
-        byte[] buffer = new byte[12];
+        byte[] buffer = new byte[8];
         random.nextBytes(buffer);
         return encoder.encodeToString(buffer);
     }
 
     public LocalDateTime createUrlExpirationDate(LocalDateTime expirationDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.GERMANY);
 
         if (expirationDate == null) {
-            return LocalDateTime.parse(LocalDateTime.now().plusMinutes(3).format(formatter),formatter) ;  //with minutes for testing only
+            return LocalDateTime.now().plusMinutes(3);  //with minutes for testing only
         } else {
-            return LocalDateTime.parse(expirationDate.toString(), formatter);
+            return LocalDateTime.parse(expirationDate.toString());
         }
     }
 }
