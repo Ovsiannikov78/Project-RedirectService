f8479fd2158b32100df23c981427c44ca7bb2517
Ovsiannikov78 <seri_tanya@mail.ru>
2021-04-15 22:03:53 +0200

    Junit test for UrlServiceImpl wit invalid date

--- UrlShortener/src/test/java/de/ovsiannikov/redirectservice/prototype/UrlsPrototype.java
+++ UrlShortener/src/test/java/de/ovsiannikov/redirectservice/prototype/UrlsPrototype.java
@@ -9,6 +9,7 @@ public class UrlsPrototype {
 
     private final static LocalDateTime testCurrentDate = LocalDateTime.of(2021,4,14,10,00,00);
     private final static LocalDateTime testDateFromUser = LocalDateTime.of(2021,8,10,12,00,00);
+    private final static LocalDateTime testInvalidDateFromUser = LocalDateTime.of(2020,8,10,12,00,00);
 
 
     public static Url urlWithoutExpirationDateFromUser() {
@@ -28,6 +29,16 @@ public class UrlsPrototype {
         url.setShortUrl("test123");
         url.setLongUrl("https://www.test123");
         url.setExpirationDate(testDateFromUser);
+        return url;
+    }
+
+    public static Url urlWithInvalidExpirationDateFromUser() {
+        Url url = new Url();
+        url.setId(1L);
+        url.setCustomerNumber(0);
+        url.setShortUrl("test123");
+        url.setLongUrl("https://www.test123");
+        url.setExpirationDate(testInvalidDateFromUser);
         return url;
     }
 
